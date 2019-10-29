d3b0060c59a30977348ebc4d96e0d62b6c47a566
Ethan Lee <ethanlee515@gmail.com>
2019-10-29 18:15:14 +0800

    Add progress on Project Hentai

--- site_Project_Hentai.py
+++ site_Project_Hentai.py
@@ -1,25 +1,42 @@
 #!/usr/bin/env python3.7
 name = "Project Hentai"
 
-import binascii
 import re
 import sys
 import asyncio
-from bs4 import BeautifulSoup
 import requests
 
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 async def get_manga_by_author(author):
-	params = f'search?keyword={author}'
-	hex_params = binascii.hexlify(str.encode(params))
-	url = f'https://www.projecthentai.com/online-store/hex%3A{hex_params}'
+	options = Options()
+	options.headless = True
+	driver = webdriver.Firefox(options=options)
+	driver.implicitly_wait(10)
+	driver.get('https://www.projecthentai.com')
+	results = list()
+	driver.find_element_by_css_selector(
+			'//iframe//.ecwid-search-widget__input') \
+			.send_keys(author, Keys.ENTER)
+	url = driver.getCurrentUrl()
+
 	
-	site = requests.get(url).text
-	# TODO The above doesn't work because the site is rendered dynamically
+
+
+
+	# TODO The above doesn't work because of iframe shenanigans
+	'''
 	soup = BeautifulSoup(site, 'html.parser')
 	results_count = int(re.search(r"of (\d+) items",
 		soup.find(class_='pager__count-pages')).group(1))
+	'''
 
-	return results_count
+	driver.quit()
+
+	return url
 	
 	# params += '&offset=60'
 
