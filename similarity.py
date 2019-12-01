07bd0520876fd3a949d7b20b4ac8cf637563d8f7
Ethan Lee <ethanlee515@gmail.com>
2019-12-01 22:29:25 +0800

    Update universal sentence encoder version

--- similarity.py
+++ similarity.py
@@ -2,27 +2,28 @@
 
 print("Loading Tensorflow...")
 # IF using tensorflow 2.0+
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+# import tensorflow.compat.v1 as tf
+# tf.disable_v2_behavior()
 # ELSE
-# import tensorflow as tf
+import tensorflow as tf
 # ENDIF
 
 import tensorflow_hub as hub
 import numpy as np
 
 print("Loading Tensorflow module...")
-embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
+embed = hub.KerasLayer("https://tfhub.dev/google/universal-sentence-encoder/3")
 print("Tensorflow module loaded")
 
-session = tf.Session()
-session.run(tf.global_variables_initializer())
-session.run(tf.tables_initializer())
+session = tf.compat.v1.Session()
+session.run(tf.compat.v1.global_variables_initializer())
+session.run(tf.compat.v1.tables_initializer())
 
 def compute_similarity(title1, title2):
 	vecs = session.run(embed([title1, title2]))
 	arrs = [np.array(vecs[i]) for i in (0, 1)]
 	return np.inner(arrs[0], arrs[1])
+
 	
 if __name__ == "__main__":
 	sentences = [
@@ -32,6 +33,10 @@ if __name__ == "__main__":
 		"Your cellphone looks great.",
 		"Hi, how is it going?",
 		"How are you doing?"]
+	outputs = embed(sentences)["outputs"]
+	print(outputs.eval(session=session))
+
+	'''
 	print("Testing similarities...")
 	print("Below should be three matches (closer to 1), "
 			"followed by three non-matches (closer to 0).")
@@ -41,3 +46,4 @@ if __name__ == "__main__":
 	print(compute_similarity(sentences[1], sentences[3]))
 	print(compute_similarity(sentences[3], sentences[4]))
 	print(compute_similarity(sentences[2], sentences[5]))
+	'''
