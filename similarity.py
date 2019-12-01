fe4224fe6204515105a50154707df31b33df50a0
Ethan Lee <ethanlee515@gmail.com>
2019-12-01 23:12:01 +0800

    Done with similarity module

--- similarity.py
+++ similarity.py
@@ -3,40 +3,30 @@
 import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
+import sys
+import json
 
-embed = hub.KerasLayer("https://tfhub.dev/google/universal-sentence-encoder/3")
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/3")
+
+sess = tf.compat.v1.Session()
+sess.run(tf.compat.v1.global_variables_initializer())
+sess.run(tf.compat.v1.tables_initializer())
 
 print("Tensorflow ready")
 
-session = tf.compat.v1.Session()
-session.run(tf.compat.v1.global_variables_initializer())
-session.run(tf.compat.v1.tables_initializer())
+def find_matches(title, titles):
+	titles.append(title)
+	vecs = embed(titles)["outputs"].eval(session=sess)
+	matches = list()
+	near_matches = list()
+	for i in range(len(vecs) - 1):
+		similarity = np.inner(vecs[i], vecs[-1])
+		if similarity > .9:
+			matches.append(titles[i])
+		elif similarity > .5:
+			near_matches.append(titles[i])
+	return {"matches": matches, "near_matches": near_matches}
 
-def compute_similarity(title1, title2):
-	vecs = session.run(embed([title1, title2]))
-	arrs = [np.array(vecs[i]) for i in (0, 1)]
-	return np.inner(arrs[0], arrs[1])
-
-	
-if __name__ == "__main__":
-	sentences = [
-		"How old are you?",
-		"What is your age?",
-		"I like your phone.",
-		"Your cellphone looks great.",
-		"Hi, how is it going?",
-		"How are you doing?"]
-	outputs = embed(sentences)["outputs"]
-	print(outputs.eval(session=session))
-
-	'''
-	print("Testing similarities...")
-	print("Below should be three matches (closer to 1), "
-			"followed by three non-matches (closer to 0).")
-	print(compute_similarity(sentences[0], sentences[1]))
-	print(compute_similarity(sentences[2], sentences[3]))
-	print(compute_similarity(sentences[4], sentences[5]))
-	print(compute_similarity(sentences[1], sentences[3]))
-	print(compute_similarity(sentences[3], sentences[4]))
-	print(compute_similarity(sentences[2], sentences[5]))
-	'''
+for line in sys.stdin:
+	req = json.loads(line)
+	print(json.dumps(find_matches(req["title"], req["titles"])))
