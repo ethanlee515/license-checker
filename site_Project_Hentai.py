c2986ff25556ef9a1c7c0b9b14aac40013c298a1
Ethan Lee <ethanlee515@gmail.com>
2020-01-01 07:28:37 +0000

    Fix Project Hentai subprocess bug

--- site_Project_Hentai.py
+++ site_Project_Hentai.py
@@ -6,18 +6,11 @@ import asyncio
 name = "Project Hentai"
 async def get_manga_by_author(author):
 	proc = await asyncio.create_subprocess_exec(
-			"./site_Project_Hentai.py",
+			"./Project_Hentai.py",
 			author,
 			stdout=asyncio.subprocess.PIPE)
-	await proc.wait()
-	results = list()
-	while True:
-		line = (await proc.stdout.readline()).decode().strip()
-		if len(line) == 0:
-			break
-		results.append(line)
-	return results
-	
+	stdout, stderr = await proc.communicate()
+	return stdout.decode().split('\n')
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
