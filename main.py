ad6b65ca66783836e9c12e796681ab8ddb85d481
Stinggy <stinggyray@gmail.com>
2020-01-09 23:30:16 -0600

    who cares about PEP-8 anyway?

--- main.py
+++ main.py
@@ -68,8 +68,8 @@ async def on_message(msg):
 			elif match2.group(i) == 't':
 				title = match2.group(i + 1).strip().strip('"')
 	if author is None or len(author) == 0 or title is None or len(title) == 0:
-		asyncio.create_task(msg.channel.send('Invalid command. '
-											 'Usage: `.lc "author" "title"` or `.lc -a author -t title`'))
+		asyncio.create_task(
+			msg.channel.send('Invalid command. Usage: `.lc "author" "title"` or `.lc -a author -t title`'))
 		return
 	await msg.channel.send(f"Looking up {title} by {author}.")
 	for site in site_modules:
