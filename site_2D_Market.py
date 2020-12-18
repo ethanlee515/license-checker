2a6e0b49322fa4aac26954c5ac8947b58d3bce8c
Ethan Lee <ethanlee515@gmail.com>
2020-12-18 15:37:05 +0000

    upgrade to python3.9

--- site_2D_Market.py
+++ site_2D_Market.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python3.9
 
 import sys
 import asyncio
@@ -39,6 +39,6 @@ async def get_manga_by_author(author):
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
-		print('usage: python3.8 site_2D_Market.py "author name"')
+		print('usage: python3.9 site_2D_Market.py "author name"')
 		exit(1)
 	print(asyncio.run(get_manga_by_author(sys.argv[1])))
